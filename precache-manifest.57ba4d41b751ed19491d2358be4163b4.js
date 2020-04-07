e0e72789435261342c756c57b424451f25ee6552
ManliestBen <benjamintmanley@gmail.com>
2020-04-07 16:32:59 -0500

    Updates

---
+++ precache-manifest.57ba4d41b751ed19491d2358be4163b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e712c5d9614b1d89719787ab4f104711",
+    "url": "/react-pokemon-api/index.html"
+  },
+  {
+    "revision": "00b2aabad18c8fe217d2",
+    "url": "/react-pokemon-api/static/css/main.9a042cdf.chunk.css"
+  },
+  {
+    "revision": "35ca00bd64a5bb05702d",
+    "url": "/react-pokemon-api/static/js/2.8d630802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokemon-api/static/js/2.8d630802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b2aabad18c8fe217d2",
+    "url": "/react-pokemon-api/static/js/main.4b19c93c.chunk.js"
+  },
+  {
+    "revision": "91159509db4200b28185",
+    "url": "/react-pokemon-api/static/js/runtime-main.b590305f.js"
+  }
+]);
