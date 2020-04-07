31c75f77d4a86f904b6ea448d1d9f496aae6f75b
ManliestBen <benjamintmanley@gmail.com>
2020-04-07 16:14:45 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import { getAllPokemon } from './services/pokemon-api';
+import PokemonPage from './pages/PokemonPage/PokemonPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    pokemon: []
+  };
+
+  getPokemon = (idx) => {
+    return this.state.pokemon[idx];
+  }
+
+  async componentDidMount() {
+    const pokemon = await getAllPokemon();
+    console.log(pokemon.results);
+    this.setState({ pokemon: pokemon.results });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Pokemon Index</header>
+        <Switch>
+        <Route exact path='/' render={() => 
+            <section>
+              {this.state.pokemon.map((pokemon, idx) => 
+                <Link
+                  to={`/pokemon/${idx}`}
+                  key={pokemon.name}
+                >
+                  {pokemon.name}
+                </Link>
+              )}
+            </section>
+          }/>
+          <Route path='/pokemon/:idx' render={(props) => 
+            <PokemonPage
+              {...props}
+              getPokemon={this.getPokemon}
+            />
+          }/>
+          </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/services/pokemon-api.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+export function getAllPokemon() {
+    return fetch(`${BASE_URL}?limit=1000`, {mode: "cors"})
+    .then(res => res.json());
+}
+
+export function getPokemonDetails(idx) {
+    return fetch(`${BASE_URL}${idx}`, {mode: "cors"})
+    .then(res => res.json());
+}
